a51af5786c4ebd14263e282ebc142758c2c9cfaa
Moka <akunrusak720@gmail.com>
2024-01-23 18:35:52 +0700

    unit-test 1

---
+++ app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
@@ -0,0 +1,34 @@
+package belajar.kotlin.unit.test
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.*
+
+class CalculatorTest {
+    val calculator = Calculator()
+
+    @Test
+    fun testAddSuccess(){
+        val result = calculator.add(10, 10)
+        assertEquals(20, result, "Hasil harusnya 20")
+    }
+
+    @Test
+    fun testAddSuccess1(){
+        val result = calculator.add(20, 20)
+        assertEquals(40, result, "Hasil harus 40")
+    }
+
+    @Test
+    fun testDivideSuccess(){
+        val result = calculator.divide(100, 10)
+        assertEquals(10, result);
+    }
+
+    @Test
+    fun testDivideFailed(){
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(100, 0)
+        }
+    }
+}
